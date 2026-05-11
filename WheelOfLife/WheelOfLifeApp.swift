e85724aa349430d167615d3f5473d2e6fb0fecfe
Peter Davies <p.davies@austinconsultants.co.uk>
2026-05-11 01:33:25 +0100

    Initial Commit

---
+++ WheelOfLife/WheelOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelOfLifeApp.swift
+//  WheelOfLife
+//
+//  Created by Peter Davies on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WheelOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WheelOfLifeTests/WheelOfLifeTests.swift
@@ -0,0 +1,19 @@
+//
+//  WheelOfLifeTests.swift
+//  WheelOfLifeTests
+//
+//  Created by Peter Davies on 11/05/2026.
+//
+
+import Testing
+@testable import WheelOfLife
+
+struct WheelOfLifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
